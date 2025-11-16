fdfcdc861b389be4a74e13ac93f8538007d4cdf3
Timothy Lott <timbo245@users.noreply.github.com>
2025-11-16 11:40:41 -0800

    fixed module error message

--- app/api/reports/state-appointment-history/excel/[state]/route.ts
+++ app/api/reports/state-appointment-history/excel/[state]/route.ts
@@ -1,32 +1,32 @@
-// import clientPromise from '@/app/lib/mongodb';
-// import { NextResponse } from 'next/server';
+import clientPromise from '@/app/lib/mongodb';
+import { NextResponse } from 'next/server';
 
-// //TODO: FIX. RE{PRTOMG OSSIE}
+//TODO: FIX. RE{PRTOMG OSSIE}
 
-// export async function GET(
-//   _req: Request,
-//   { params }: { params: Promise<{ state: string }> }
-// ) {
-//   const { state } = await params;
-//   const rawState = state.replace(/-/g, ' ');
-//   const normalized = rawState.trim().replace(/\s+/g, ' ');
+export async function GET(
+  _req: Request,
+  { params }: { params: Promise<{ state: string }> }
+) {
+  const { state } = await params;
+  const rawState = state.replace(/-/g, ' ');
+  const normalized = rawState.trim().replace(/\s+/g, ' ');
 
-//   const client = await clientPromise;
-//   const db = client.db(process.env.MONGO_DB);
+  const client = await clientPromise;
+  const db = client.db(process.env.MONGO_DB);
 
-//   const appointmentHistory = await db
-//     .collection('appointmentHistory')
-//     .find({
-//       state: {
-//         $regex: `^\\s*${normalized.replace(
-//           /[.*+?^${}()|[\]\\]/g,
-//           '\\$&'
-//         )}\\s*$`,
-//         $options: 'i'
-//       }
-//     })
-//     .sort({ appointmentDate: -1 })
-//     .toArray();
+  const appointmentHistory = await db
+    .collection('appointmentHistory')
+    .find({
+      state: {
+        $regex: `^\\s*${normalized.replace(
+          /[.*+?^${}()|[\]\\]/g,
+          '\\$&'
+        )}\\s*$`,
+        $options: 'i'
+      }
+    })
+    .sort({ appointmentDate: -1 })
+    .toArray();
 
-//   return NextResponse.json({ appointmentHistory });
-// }
+  return NextResponse.json({ appointmentHistory });
+}
